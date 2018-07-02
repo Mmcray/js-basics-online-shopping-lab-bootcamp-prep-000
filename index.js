1af2f4c36817c7cf33a41b0b709a599b98eb1e95
Michael McRay <mmcray5@gmail.com>
2018-07-02 16:16:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,5 +65,5 @@ function placeOrder(cardNumber) {
   else { 
      return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   }
-  cart.length = 0;
+  let cart = [];
 }
